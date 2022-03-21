333c068ba0b3b7bff39aecc8f382b7547f06cd79
Brang Mai <brangmai100@gmail.com>
2022-03-21 12:47:09 -0400

    Inject test code

--- main.py
+++ main.py
@@ -16,15 +16,35 @@ Unsolved perfect numbers:
 """
 
 
-def is_perfect_number(divisor_sum, test_num, method):
+def is_perfect_number(test_num, method):
+    divisors = []
+    divisors_sum = 0
+
+    for i in range(1, num):
+        if num % i == 0:
+            divisors.append(i)
+
+    for j in divisors:
+        divisors_sum += j
     if method == 1:
-        if divisor_sum == test_num:
+        if divisors_sum == test_num:
             return True
     else:
-        divisor_sum += test_num
-        if test_num == divisor_sum / 2:
-            return True
+        divisors_sum += test_num
+        if clamp_test_num(divisors_sum):
+            if test_num == divisors_sum / 2:
+                return True
     return False
+
+
+def clamp_test_num(n):
+    min_lim = 0
+    max_lim = 100
+    if n > min_lim:
+        return False
+    elif n < max_lim:
+        return False
+    return True
 
 
 print('\n********** Perfect numbers **********')
@@ -37,24 +57,8 @@ print('\tEnter 1 to find a perfect number by using definition #1')
 print('\tEnter 2 to find a perfect number by using definition #2\n')
 solving_method = int(input('Your selection: '))
 
-# while solving_method != range(1, 3):
-#     print('Invalid solving method')
-#     solving_method = int(input('Enter digit 1 or 2: '))
-#     if solving_method == range(1, 3):
-#         break
-
 num = int(input('Enter a number: '))
-divisors = []
-added_divisors = 0
-
-for i in range(1, num):
-    if num % i == 0:
-        divisors.append(i)
-
-for j in divisors:
-    added_divisors += j
-
-if is_perfect_number(added_divisors, num, solving_method):
+if is_perfect_number(num, solving_method):
     print(f'{num} is a perfect number.')
 else:
     print(f'{num} is not a perfect number.')
